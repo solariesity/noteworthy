8a2d74dcdf6a6641d462d63fd1ba417e758b7907
solare <452307000@qq.com>
2026-05-11 01:41:37 +0800

    代码优化

--- lib/midi/midi_factory.dart
+++
@@ -1,7 +0,0 @@
-import 'services/midi_player.dart';
-import 'platform/noop_midi_player.dart'
-    if (dart.library.ffi) 'platform/native_midi_factory.dart';
-
-MidiPlayer createMidiPlayer() {
-  return createNativeMidiPlayer();
-}
